eb9bccb1d72ecfabc79ab99db8a565d6791aa304
AK2b_9yMgt*9uKj-m <mahmud102496@gmail.com>
2026-09-25 04:19:38 +0600

    fix cart-1

--- script/video.js
+++ script/video.js
@@ -39,29 +39,10 @@ const displaycatagory = (categories) => {
 const lodevideos = () => {
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
         .then((res) => res.json())
-        .then((data) =>displayvideos(data.videos))
+        .then((data) => displayvideos(data.videos))
         .catch((error) => console.error("Error fetching videos:", error));
 };
 
-
-const carddemo = {
-    "category_id": "1001",
-    "video_id": "aaab",
-    "thumbnail": "https://i.ibb.co/QPNzYVy/moonlight.jpg",
-    "title": "Midnight Serenade",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/fDbPv7h/Noha.jpg",
-            "profile_name": "Noah Walker",
-            "verified": false
-        }
-    ],
-    "others": {
-        "views": "543K",
-        "posted_date": ""
-    },
-    "description": "'Midnight Serenade' by Noah Walker is a soulful journey into the depths of the night, capturing the mystique and allure of a moonlit evening. With 543K views, this song brings together tender melodies and evocative lyrics, making it a favorite among listeners seeking a contemplative yet uplifting experience. Immerse yourself in this musical masterpiece and feel the calm embrace of the night."
-}
 const displayvideos = (videos) => {
 
     const videoContainer = document.getElementById("videos");
@@ -70,19 +51,26 @@ const displayvideos = (videos) => {
         console.log(video);
         const card = document.createElement("div");
         card.classList = "card card-compact";
-        card.innerHTML = `<figure>
+        card.innerHTML = `<figure class="h-[300px] w-full">
     <img
       src=${video.thumbnail}
+      class="h-full w-full object-cover"
       alt="Shoes" />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">${video.title}</h2>
-    <p>${video.description}</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Play</button>
+  <div class="px-0 py-2 flex flex-col gap-2">
+        <div>
+        <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}" alt="" />
+        </div>
+    <div>
+        <h2 class="font-bold">${video.title}</h2>
+        <div class="flex items-center gap-2">
+            <p class="text-gray-500">${video.authors[0].profile_name}</p>
+            <img class="w-5" src="https://img.icons8.com/fluency/48/verified-badge--v1.png" alt="verified-badge--v1"/>
+        </div>
     </div>
+    
   </div> `;
-  videoContainer.append(card);
+        videoContainer.append(card);
 
 
     });
