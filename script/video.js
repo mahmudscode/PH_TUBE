9ebc7e94f2623e51f7397ee10ce3a87029d97db1
AK2b_9yMgt*9uKj-m <mahmud102496@gmail.com>
2026-09-26 03:41:57 +0600

    varifide or not

--- script/video.js
+++ script/video.js
@@ -10,16 +10,10 @@ const lodecatagory = () => {
         .then((data) => displaycatagory(data.categories))
         .catch((error) => console.error("Error fetching categories:", error));
 };
-
-
-
 //create display catagoris
-
 const displaycatagory = (categories) => {
     //add data in html
-
     const categoryContainer = document.getElementById("catagoris");
-
     categories.forEach((items) => {
         console.log(items);
         //create a button
@@ -32,10 +26,7 @@ const displaycatagory = (categories) => {
 
     })
 };
-
 //display video
-
-
 const lodevideos = () => {
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
         .then((res) => res.json())
@@ -51,21 +42,23 @@ const displayvideos = (videos) => {
         console.log(video);
         const card = document.createElement("div");
         card.classList = "card card-compact";
-        card.innerHTML = `<figure class="h-[300px] w-full">
-    <img
+        card.innerHTML = `<figure class="h-[300px] w-full relative">
+        <img
       src=${video.thumbnail}
       class="h-full w-full object-cover"
       alt="Shoes" />
-  </figure>
-  <div class="px-0 py-2 flex flex-col gap-2">
+      <span class="absolute right-2 bottom-2 bg-black text-white p-1 rounded">
+      ${video.others.posted_date}</span>
+    </figure>
+        <div class="px-0 py-2 flex flex-col gap-2">
         <div>
         <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}" alt="" />
         </div>
-    <div>
+        <div>
         <h2 class="font-bold">${video.title}</h2>
         <div class="flex items-center gap-2">
             <p class="text-gray-500">${video.authors[0].profile_name}</p>
-            <img class="w-5" src="https://img.icons8.com/fluency/48/verified-badge--v1.png" alt="verified-badge--v1"/>
+           ${video.authors[0].verified ? ` <img class="w-5" src="https://img.icons8.com/fluency/48/verified-badge--v1.png" alt="verified-badge--v1"/>`: ""}
         </div>
     </div>
     
